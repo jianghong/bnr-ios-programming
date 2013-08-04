5efae81614ae3d302de83f5161b1deabde381e35
Jackson <jackson.hong@kontagent.com>
2013-08-04 11:10:37 -0400

    init random possessions

---
+++ RandomPossessions/RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by Jackson on 2013-08-04.
+//  Copyright (c) 2013 pandabox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
