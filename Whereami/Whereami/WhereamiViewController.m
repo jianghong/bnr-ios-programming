7ddbd9a7881e20797dd90e91cb8b85cb5472d78d
Jackson Hong <jackson.hong@kontagent.com>
2013-08-05 16:33:52 -0400

    locationManager and CLLocationManagerDelegate

--- Whereami/Whereami/WhereamiViewController.m
+++ Whereami/Whereami/WhereamiViewController.m
@@ -14,16 +14,42 @@
 
 @implementation WhereamiViewController
 
-- (void)viewDidLoad
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        // Create location manager object
+        locationManager = [[CLLocationManager alloc] init];
+        
+        [locationManager setDelegate:self];
+        
+        // And we want it to be as accurate as possible
+        // regardless of how much time/power it takes
+        [locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+        
+        // Tell the manager to start looking for its location immediately
+        [locationManager startUpdatingLocation];
+    }
+    
+    return self;
 }
 
-- (void)didReceiveMemoryWarning
+- (void)locationManager:(CLLocationManager *)manager
+didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSLog(@"%@", newLocation);
 }
 
+- (void)locationManager:(CLLocationManager *)manager
+didFailWithError:(NSError *)error
+{
+    NSLog(@"Could not find location: %@", error);
+}
+
+- (void)dealloc
+{
+    // Tell the location manager to stop sending us messages
+    [locationManager setDelegate:nil];
+}
 @end
