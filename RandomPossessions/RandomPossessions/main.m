d34d87df1772a9be460d32ab88149fe637e7daa5
Jackson <jackson.hong@kontagent.com>
2013-08-04 21:12:56 -0400

    Weak reference created from calculator to backpack

--- RandomPossessions/RandomPossessions/main.m
+++ RandomPossessions/RandomPossessions/main.m
@@ -14,22 +14,21 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // Create a mutable array object, store its address in items variable
-        NSMutableArray *items = [[NSMutableArray alloc] init];
        
         BNRItem *backpack = [[BNRItem alloc] init];
         [backpack setItemName:@"Backpack"];
-        [items addObject:backpack];
         
         BNRItem *calculator = [[BNRItem alloc] init];
         [calculator setItemName:@"Calculator"];
-        [items addObject:calculator];
         
         [backpack setContainedItem:calculator];
         
         // Destroy the array pointed to by items
         NSLog(@"Setting items to nil...");
-        items = nil;
+        
+        backpack = nil;
+        NSLog(@"Container: %@", [calculator container]);
+        calculator = nil;
         
     }
     return 0;
