475acc2e52690936a477452b42ff2f65880e8fbe
Jackson Hong <jackson.hong@kontagent.com>
2013-08-07 10:46:58 -0400

    Two HypnosisViews with drawn

---
+++ Hypnosister/Hypnosister/HypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Jackson Hong on 2013-08-07.
+//  Copyright (c) 2013 Pandabox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HypnosisView : UIView
+
+@end
---
+++ Hypnosister/Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Jackson Hong on 2013-08-07.
+//  Copyright (c) 2013 Pandabox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
