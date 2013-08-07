d5f31ea00a4e59b75d6212b466dcb3413d94f1ff
Jackson Hong <jackson.hong@kontagent.com>
2013-08-07 10:57:54 -0400

    hypnosis drawn on full size of window

--- Hypnosister/Hypnosister/HypnosisterAppDelegate.m
+++ Hypnosister/Hypnosister/HypnosisterAppDelegate.m
@@ -16,17 +16,11 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
-    CGRect viewFrame = CGRectMake(160, 240, 100, 150);
     
-    HypnosisView *view = [[HypnosisView alloc] initWithFrame:viewFrame];
+    HypnosisView *view = [[HypnosisView alloc] initWithFrame:[[self window] bounds]];
     
     [[self window] addSubview:view];
-    
-    CGRect anotherFrame = CGRectMake(20, 30, 50, 50);
-    HypnosisView *anotherView = [[HypnosisView alloc] initWithFrame:anotherFrame];
-    
-    [view addSubview:anotherView];
-    
+       
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
